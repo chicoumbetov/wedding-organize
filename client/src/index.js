a57186642e156a915bf94ff0c58f241d06917bcf
chicoumbetov <shyngysmaaan@gmail.com>
2021-11-14 17:41:23 +0100

    Create store, reducers, actions and connect to app

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+
+import reducers from './reducers'
 
 import App from './App';
+import {applyMiddleware, compose, createStore} from "redux";
+import thunk from "redux-thunk";
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
