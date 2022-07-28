fe63e617dc3bf19c593c27ac3cce3440c21a9e01
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-07-28 23:10:29 +0200

    :bug: Fix Admin component

--- client/src/components/Admin/Admin.tsx
+++ client/src/components/Admin/Admin.tsx
@@ -1,54 +1,57 @@
-import React, {useEffect, useState} from "react"
-import {Col, Row} from "react-bootstrap";
-import {getUsersThunk} from "../../redux/thunk";
-import {useAppDispatch, useAppSelector} from "../../store/hooks";
-import {statusSelector, usersSelector} from "../../store/selectors";
+import React, { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import { getUsersThunk } from "../../redux/thunk";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { usersSelector } from "../../store/selectors";
 
 // function useQuery() { return new URLSearchParams(useLocation().search) }
 
 const Admin = () => {
-    const [ user ] = useState(JSON.parse(localStorage.getItem('profile') || '{}'))
-    // const query = useQuery();
-    // const searchQuery = query.get('searchQuery');
+  const [user] = useState(JSON.parse(localStorage.getItem("profile") || "{}"));
+  // const query = useQuery();
+  // const searchQuery = query.get('searchQuery');
 
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const reduxUsers = useAppSelector(usersSelector);
+  const reduxUsers = useAppSelector(usersSelector);
 
-    useEffect(() => {
-        dispatch(getUsersThunk());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getUsersThunk());
+  }, [dispatch]);
 
-    const haveRigthToModify = () => {
-        if(
-            user && user?.isAdmin
-            // mongo db /* user.result && user.result.isAdmin */
-        ) {
-            return(<div> Have rights to modify </div>)
-        } else {
-            return(<div> No rights to modify </div>)
-        }
+  const haveRigthToModify = () => {
+    if (
+      user &&
+      user?.isAdmin
+      // mongo db /* user.result && user.result.isAdmin */
+    ) {
+      return <div> Have rights to modify </div>;
+    } else {
+      return <div> No rights to modify </div>;
     }
+  };
 
-    return (
-        <div>
-            {haveRigthToModify()}
-            {
-                reduxUsers
-                && reduxUsers.map((user) => {
-                    return(
-                            React.Children.toArray(
-                                <Row lg={12} style={{ display: 'flex'}}>
-                                    <Col lg={6} style={{ flex: '1', marginRight: "50px"}}>{user.username}</Col>
-                                    <Col lg={6} style={{ flex: '1', marginRight: "50px"}}>{user.email}</Col>
-                                    <Col lg={6} style={{ flex: '1' }}>{user.isAdmin ? "Admin" : "Not admin"}</Col>
-                                </Row>
-                            )
-                        )
-                })
-           }
-        </div>
-    )
-}
+  return (
+    <div>
+      {haveRigthToModify()}
+      {reduxUsers &&
+        reduxUsers.map((user) => {
+          return React.Children.toArray(
+            <Row lg={12} style={{ display: "flex" }}>
+              <Col lg={6} style={{ flex: "1", marginRight: "50px" }}>
+                {user.username}
+              </Col>
+              <Col lg={6} style={{ flex: "1", marginRight: "50px" }}>
+                {user.email}
+              </Col>
+              <Col lg={6} style={{ flex: "1" }}>
+                {user.isAdmin ? "Admin" : "Not admin"}
+              </Col>
+            </Row>
+          );
+        })}
+    </div>
+  );
+};
 
 export default Admin;
