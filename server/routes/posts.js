b89005423b0152f48dc10f67839f7dc15ff43343
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-07-29 12:56:08 +0200

    :bug: Server posts routes fix

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,19 +1,28 @@
 // controllers
-const { getPostsBySearch, getPost, getPosts, createPost, updatePost, deletePost, likePost, commentPost } = require("../controllers/posts.js");
-const auth = require('../middleware/auth.js');
+const {
+  getPostsBySearch,
+  getPost,
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+  commentPost,
+} = require("../controllers/posts.js");
+const auth = require("../middleware/auth.js");
 
 const express = require("express");
 const router = express.Router();
 
 // reached by localhost:5000/posts
-router.get('/search', getPostsBySearch);
-router.get('/', getPosts);
-router.get('/:id', getPost);
+router.get("/search", getPostsBySearch);
+router.get("/", getPosts);
+router.get("/:id", getPost);
 
-router.post('/', auth, createPost);
-router.patch('/:id', auth, updatePost);
-router.patch('/:id/likePost', auth, likePost);
-router.post('/:id/commentPost', auth, commentPost);
-router.delete('/:id', auth, deletePost)
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.patch("/:id/likePost", auth, likePost);
+router.post("/:id/commentPost", auth, commentPost);
+router.delete("/:id", auth, deletePost);
 
 module.exports = router;
